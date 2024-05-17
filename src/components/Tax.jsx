a952f940dafe99c4be0a658b5b1ee6370b978616
Nico Gambolati <nicolasgambolati@gmail.com>
2024-05-16 21:06:56 -0300

    rounding of the sub total

--- src/components/Tax.jsx
+++ src/components/Tax.jsx
@@ -5,12 +5,19 @@ export const Tax = ({ tax, setTax, subTotal, setTotal }) => {
       newtax = (subTotal - 50) * 0.5;
     }
 
-    setTax(newtax);
+    setTax(roundingResult(newtax));
     setTotal(subTotal + newtax);
   };
+
+  const roundingResult = (num) => {
+    return Math.round((num + Number.EPSILON) * 100) / 100;
+  };
+
   return (
     <div className="d-flex flex-column align-items-center gap-3 ">
-      <button onClick={calculateTax} className="btn btn-primary">Calcular Impuesto</button>
+      <button onClick={calculateTax} className="btn btn-primary">
+        Calcular Impuesto
+      </button>
       <h6>Impuesto Aduanero: ${tax}</h6>
     </div>
   );
